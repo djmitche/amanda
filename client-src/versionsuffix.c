c764ecbe999e3687bd368ef85555acd76b13501e
Amanda Core <amanda-hackers@amanda.org>
1997-06-28 00:16:28 +0000

    Libtoolized amanda.  In order to update automatically generated files (particularly aclocal.m4), at least libtool.m4, from the libtool distribution, must be installed in /usr/local/share/aclocal.

--- client-src/versionsuffix.c
+++ client-src/versionsuffix.c
@@ -1,6 +1,8 @@
 #include "amanda.h"
 #include "version.h"
 
+char *pname = "versionsuffix";
+
 int main()
 {
 	printf("%s\n", versionsuffix());
--- recover-src/uparse.c
+++ recover-src/uparse.c
@@ -176,7 +176,7 @@ static const short yycheck[] = {     3,
     20,    20,    20,    20,    20,    20,     0,     0
 };
 /* -*-C-*-  Note some compilers choke on comments on `#line' lines.  */
-#line 3 "/opt/util/share/bison.simple"
+#line 3 "/n/gnu/bison-1.25/share/bison.simple"
 
 /* Skeleton output parser for bison,
    Copyright (C) 1984, 1989, 1990 Free Software Foundation, Inc.
@@ -369,7 +369,7 @@ __yy_memcpy (char *to, char *from, int count)
 #endif
 #endif
 
-#line 196 "/opt/util/share/bison.simple"
+#line 196 "/n/gnu/bison-1.25/share/bison.simple"
 
 /* The user can define YYPARSE_PARAM as the name of an argument to be passed
    into yyparse.  The argument should have type void *.
@@ -755,7 +755,7 @@ case 30:
     break;}
 }
    /* the action file gets copied in in place of this dollarsign */
-#line 498 "/opt/util/share/bison.simple"
+#line 498 "/n/gnu/bison-1.25/share/bison.simple"
 
   yyvsp -= yylen;
   yyssp -= yylen;
