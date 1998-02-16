78a43efd3c7baacffdd144f694d127da68876659
Amanda Core <amanda-hackers@amanda.org>
1998-02-16 09:29:19 +0000

    one more error SAMBA error message that might be disregarded

--- client-src/sendbackup-gnutar.c
+++ client-src/sendbackup-gnutar.c
@@ -25,7 +25,7 @@
  *			   University of Maryland at College Park
  */
 /* 
- * $Id: sendbackup-gnutar.c,v 1.48 1998/02/16 09:06:24 amcore Exp $
+ * $Id: sendbackup-gnutar.c,v 1.49 1998/02/16 09:29:19 amcore Exp $
  *
  * send backup data using GNU tar
  */
@@ -96,6 +96,7 @@ static regex_t re_table[] = {
    * your own risk! */
   { DMP_NORMAL, "^ERRDOS - ERRbadshare opening remote file", 1},
   { DMP_NORMAL, "^ERRDOS - ERRbadfile opening remote file", 1},
+  { DMP_NORMAL, "^ERRDOS - ERRnoaccess setting attributes on file", 1},
 #endif
 
   /* catch-all: DMP_STRANGE is returned for all other lines */
