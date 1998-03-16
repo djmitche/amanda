790888f0bac4113cd6cecd2fd776e947921b289a
Amanda Core <amanda-hackers@amanda.org>
1998-03-16 10:37:56 +0000

    when amrecover starts up, it will chdir() to the root of the local filesystem, if the local directory exists in the index.

--- recover-src/amrecover.c
+++ recover-src/amrecover.c
@@ -25,7 +25,7 @@
  *			   University of Maryland at College Park
  */
 /*
- * $Id: amrecover.c,v 1.24 1998/03/07 18:11:49 martinea Exp $
+ * $Id: amrecover.c,v 1.25 1998/03/16 10:37:56 amcore Exp $
  *
  * an interactive program for recovering backed-up files
  */
@@ -569,6 +569,7 @@ char **argv;
 	exit(1);
     }
 
+#if 0
     /*
      * We may need root privilege again later for a reserved port to
      * the tape server, so we will drop down now but might have to
@@ -576,6 +577,7 @@ char **argv;
      */
     setegid(getgid());
     seteuid(getuid());
+#endif
 
     if (connect(server_socket, (struct sockaddr *)&server, sizeof(server))
 	== -1)
@@ -670,8 +672,14 @@ char **argv;
 		    printf("$CWD '%s' is on disk '%s' mounted at '%s'.\n",
 			   cwd, dn_guess, mpt_guess);
 		    set_disk(dn_guess, mpt_guess);
-		    afree(mpt_guess);
 		    set_directory(cwd);
+		    if (server_happy()) {
+			if (chdir(mpt_guess) == 0)
+			    printf("$LCWD '%s'.\n", mpt_guess);
+			else
+			    printf("Cannot lcd to '%s'.\n", mpt_guess);
+		    }
+		    afree(mpt_guess);
 		    break;
 
 		case 0:
