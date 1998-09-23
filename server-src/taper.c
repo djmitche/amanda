485e4d9cee74bda18b2d209e77289bfb77912972
oliva <oliva@users.sourceforge.net>
1998-09-23 03:27:58 +0000

    * server-src/taper.c (getcmd): typo in last DEBUGging patch

--- server-src/taper.c
+++ server-src/taper.c
@@ -23,7 +23,7 @@
  * Authors: the Amanda Development Team.  Its members are listed in a
  * file named AUTHORS, in the root directory of this distribution.
  */
-/* $Id: taper.c,v 1.38.2.2 1998/09/20 09:06:52 oliva Exp $
+/* $Id: taper.c,v 1.38.2.3 1998/09/23 03:27:58 oliva Exp $
  *
  * moves files from holding disk to tape, or from a socket to tape
  */
@@ -1196,7 +1196,7 @@ getcmd(argcp, argvp)
       int arg;
       fprintf(stderr, "argc = %d\n", argc);
       for(arg = 0; arg <= argc; arg++)
-	printf(stderr, "argv[%d] = \"%s\"\n", arg, argv[arg]);
+	fprintf(stderr, "argv[%d] = \"%s\"\n", arg, argv[arg]);
     }
 #endif
 
