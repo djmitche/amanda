c6f94463bfc45334a140c1bb0312f7f367871fed
Jean-Louis Martineau <martineau@zmanda.com>
1998-11-18 23:51:17 +0000

    * server-src/dumper.c (main): reduce the size of the chunksize by 	2 blocks.

--- server-src/dumper.c
+++ server-src/dumper.c
@@ -23,7 +23,7 @@
  * Authors: the Amanda Development Team.  Its members are listed in a
  * file named AUTHORS, in the root directory of this distribution.
  */
-/* $Id: dumper.c,v 1.75 1998/11/03 21:40:12 jrj Exp $
+/* $Id: dumper.c,v 1.75.2.1 1998/11/18 23:51:17 martinea Exp $
  *
  * requests remote amandad processes to dump filesystems
  */
@@ -272,7 +272,7 @@ char **main_argv;
 	    level = atoi(argv[6]);
 	    dumpdate = newstralloc(dumpdate, argv[7]);
 	    chunksize = atoi(argv[8]);
-	    chunksize = (((int)(chunksize/TAPE_BLOCK_SIZE))-1)*TAPE_BLOCK_SIZE;
+	    chunksize = (((int)(chunksize/TAPE_BLOCK_SIZE))-2)*TAPE_BLOCK_SIZE;
 	    progname = newstralloc(progname, argv[9]);
 	    options = newstralloc(options, argv[10]);
 	    cont_filename[0] = '\0';
