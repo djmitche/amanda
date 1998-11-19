f07b07500d61ca2accac7b4bdadcb394ec6fddb1
John R. Jackson <jrj@gandalf.cc.purdue.edu>
1998-11-19 23:13:01 +0000

    Set the default chunksize and the "0" and "-1" special chunksizes to 64 KBytes less than the maximum to allow the final read at EOF.  Leave the chunksize as it is when it comes in to dumper except to truncate it to a 32 KByte boundary.  Add some chunksize comments to the amanda.8 man page and clean up a few minor odds and ends.

--- server-src/dumper.c
+++ server-src/dumper.c
@@ -23,7 +23,7 @@
  * Authors: the Amanda Development Team.  Its members are listed in a
  * file named AUTHORS, in the root directory of this distribution.
  */
-/* $Id: dumper.c,v 1.75.2.1 1998/11/18 23:51:17 martinea Exp $
+/* $Id: dumper.c,v 1.75.2.2 1998/11/19 23:13:01 jrj Exp $
  *
  * requests remote amandad processes to dump filesystems
  */
@@ -272,7 +272,7 @@ char **main_argv;
 	    level = atoi(argv[6]);
 	    dumpdate = newstralloc(dumpdate, argv[7]);
 	    chunksize = atoi(argv[8]);
-	    chunksize = (((int)(chunksize/TAPE_BLOCK_SIZE))-2)*TAPE_BLOCK_SIZE;
+	    chunksize = (chunksize/TAPE_BLOCK_SIZE)*TAPE_BLOCK_SIZE;
 	    progname = newstralloc(progname, argv[9]);
 	    options = newstralloc(options, argv[10]);
 	    cont_filename[0] = '\0';
