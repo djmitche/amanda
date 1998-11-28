60dd1d7358d4dbd6fec0f96d417d0b2188aab06b
Jean-Louis Martineau <martineau@zmanda.com>
1998-11-28 22:56:55 +0000

    * server-src/infofile.h: declare FORCE_BUMP and FORCE_NO_BUMP 	as command. 	* server-src/amadmin.c (force_one,unforce_one,info_one): work 	with info.command with bit operator. 	* server-src/planner.c (setup_estimate): work with info.command 	with bit operator, support for FORCE_BUMP and FORCE_NO_BUMP command.

--- server-src/infofile.h
+++ server-src/infofile.h
@@ -25,7 +25,7 @@
  *			   University of Maryland at College Park
  */
 /*
- * $Id: infofile.h,v 1.9 1998/11/12 13:18:40 martinea Exp $
+ * $Id: infofile.h,v 1.10 1998/11/28 22:56:50 martinea Exp $
  *
  * interface for current info file reading code
  */
@@ -58,9 +58,11 @@ typedef struct perf_s {
 } perf_t;
 
 typedef struct info_s {
-    int  command;		/* command word */
+    unsigned int  command;		/* command word */
 #	define NO_COMMAND	0	/* no outstanding commands */
-#	define PLANNER_FORCE	1	/* force level 0 tonight */
+#	define PLANNER_FORCE	1	/* force level 0 at next run */
+#	define FORCE_BUMP	2	/* force bump at next run */
+#	define FORCE_NO_BUMP	4	/* force no-bump at next run */
     perf_t  full;
     perf_t  incr;
     stats_t inf[DUMP_LEVELS];
