a7108d6eb25f4a09df4a9b6ce8581d8ef2fa9862
Mike Grupenhoff <kashmir@munge.com>
1998-12-02 20:19:28 +0000

    * common-src/base64.{c,h}: new files, implement base64 encoding 	and decoding.  Implementation based on routines in bind, and so 	copyright is held by ISC and IBM (but is BSD-like).

---
+++ common-src/base64.h
@@ -0,0 +1,37 @@
+/*
+ * Amanda, The Advanced Maryland Automatic Network Disk Archiver
+ * Copyright (c) 1991-1998 University of Maryland at College Park
+ * All Rights Reserved.
+ *
+ * Permission to use, copy, modify, distribute, and sell this software and its
+ * documentation for any purpose is hereby granted without fee, provided that
+ * the above copyright notice appear in all copies and that both that
+ * copyright notice and this permission notice appear in supporting
+ * documentation, and that the name of U.M. not be used in advertising or
+ * publicity pertaining to distribution of the software without specific,
+ * written prior permission.  U.M. makes no representations about the
+ * suitability of this software for any purpose.  It is provided "as is"
+ * without express or implied warranty.
+ *
+ * U.M. DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING ALL
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO EVENT SHALL U.M.
+ * BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+ * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * Authors: the Amanda Development Team.  Its members are listed in a
+ * file named AUTHORS, in the root directory of this distribution.
+ */
+/*
+ * $Id: base64.h,v 1.1 1998/12/02 20:19:28 kashmir Exp $
+ *
+ * interfaces for the base64 routines
+ */
+#ifndef BASE64_H
+#define BASE64_H
+
+const char *base64encode P((const void *, size_t));
+void base64decode P((const char *, void *, size_t));
+
+#endif /* BASE64_H */
