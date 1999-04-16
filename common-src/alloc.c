8302f50d43bbe719a9f29572b3722ec1c5a21913
Mike Grupenhoff <kashmir@munge.com>
1999-04-16 15:20:37 +0000

    * common-src/alloc.c: bump copyright to 1999

--- common-src/alloc.c
+++ common-src/alloc.c
@@ -1,6 +1,6 @@
 /*
  * Amanda, The Advanced Maryland Automatic Network Disk Archiver
- * Copyright (c) 1991-1998 University of Maryland at College Park
+ * Copyright (c) 1991-1999 University of Maryland at College Park
  * All Rights Reserved.
  *
  * Permission to use, copy, modify, distribute, and sell this software and its
@@ -24,7 +24,7 @@
  * file named AUTHORS, in the root directory of this distribution.
  */
 /*
- * $Id: alloc.c,v 1.19 1999/04/16 15:18:48 kashmir Exp $
+ * $Id: alloc.c,v 1.20 1999/04/16 15:20:37 kashmir Exp $
  *
  * Memory allocators with error handling.  If the allocation fails,
  * error() is called, relieving the caller from checking the return
