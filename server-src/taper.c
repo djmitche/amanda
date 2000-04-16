e6af196d3b92217f9d5bd3673f5007b8309ffad4
Jean-Louis Martineau <martineau@zmanda.com>
2000-04-16 14:34:05 +0000

    * server-src/taper.c: don't crash if the tapelist file doesn't exist.

--- server-src/taper.c
+++ server-src/taper.c
@@ -23,7 +23,7 @@
  * Authors: the Amanda Development Team.  Its members are listed in a
  * file named AUTHORS, in the root directory of this distribution.
  */
-/* $Id: taper.c,v 1.47.2.10 2000/04/09 08:29:38 oliva Exp $
+/* $Id: taper.c,v 1.47.2.11 2000/04/16 14:34:05 martinea Exp $
  *
  * moves files from holding disk to tape, or from a socket to tape
  */
@@ -1694,8 +1694,9 @@ int label_tape()
 					".today.", cur_str, NULL);
 	}
 	if(rename(conf_tapelist, conf_tapelist_old) != 0) {
-	    error("could not rename \"%s\" to \"%s\": %s",
-		  conf_tapelist, conf_tapelist_old, strerror(errno));
+	    if(errno != ENOENT)
+		error("could not rename \"%s\" to \"%s\": %s",
+		      conf_tapelist, conf_tapelist_old, strerror(errno));
 	}
 	amfree(conf_tapelist_old);
 	if(write_tapelist(conf_tapelist)) {
