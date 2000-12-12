81f9b84639d460aed26659f59d63612c52fecb99
John R. Jackson <jrj@gandalf.cc.purdue.edu>
2000-12-12 22:16:24 +0000

    Fix Samba regex pattern to accept blanks or tabs.

--- client-src/sendbackup-gnutar.c
+++ client-src/sendbackup-gnutar.c
@@ -24,7 +24,7 @@
  * file named AUTHORS, in the root directory of this distribution.
  */
 /* 
- * $Id: sendbackup-gnutar.c,v 1.72 2000/10/11 02:23:33 martinea Exp $
+ * $Id: sendbackup-gnutar.c,v 1.73 2000/12/12 22:16:24 jrjackson Exp $
  *
  * send backup data using GNU tar
  */
@@ -91,8 +91,8 @@ static regex_t re_table[] = {
   { DMP_NORMAL, "^nread=", 1},
   { DMP_NORMAL, "^\\([0-9][0-9]* kb/s\\)", 1},
   { DMP_NORMAL, "^\\([0-9][0-9]*\\.[0-9][0-9]* kb/s\\)", 1},
-  { DMP_NORMAL, "^[ ]*[0-9][0-9]* \\([ ]*[0-9][0-9]*\\.[0-9][0-9]* kb/s\\)", 1},
-  { DMP_NORMAL, "^[ ]*directory ", 1},
+  { DMP_NORMAL, "^[ \t]*[0-9][0-9]* \\([ \t]*[0-9][0-9]*\\.[0-9][0-9]* kb/s\\)", 1},
+  { DMP_NORMAL, "^[ \t]*directory ", 1},
   { DMP_NORMAL, "^load_client_codepage", 1},
 #endif
 
