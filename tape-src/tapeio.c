1b694aff8374e534866e720cf8bba12228357ec3
John R. Jackson <jrj@gandalf.cc.purdue.edu>
2001-01-29 22:14:33 +0000

    Convert flag values to booleans so they fit in a char (compliments of Marc W. Mengel <mengel@fnal.gov>).
    Use ap_snprintf instead of snprintf (also compliments
    of Marc).

--- tape-src/tapeio.c
+++ tape-src/tapeio.c
@@ -26,7 +26,7 @@
  */
 
 /*
- * $Id: tapeio.c,v 1.20.4.7.4.1 2001/01/24 01:37:02 jrjackson Exp $
+ * $Id: tapeio.c,v 1.20.4.7.4.2 2001/01/29 22:14:33 jrjackson Exp $
  *
  * implements generic tape I/O functions
  */
@@ -623,7 +623,7 @@ tape_fsf(devname, count)
     if((fd = tape_open(devname, O_RDONLY)) < 0) {
 	r = errstr = newstralloc2(errstr, "tape open: ", strerror(errno));
     } else if(tapefd_fsf(fd, count) == -1) {
-	snprintf(count_str, sizeof(count_str), "%d", count);
+	ap_snprintf(count_str, sizeof(count_str), "%d", count);
 	r = errstr = newvstralloc(errstr,
 			          "forward skip ", count_str, "file",
 			          (count == 1) ? "" : "s",
