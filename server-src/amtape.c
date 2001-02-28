2f91fc052c21a09876b26f12d24254b9257fba1a
John R. Jackson <jrj@gandalf.cc.purdue.edu>
2001-02-28 00:34:45 +0000

    Rewind after loading a slot either by slot or by label.

--- server-src/amtape.c
+++ server-src/amtape.c
@@ -24,7 +24,7 @@
  * file named AUTHORS, in the root directory of this distribution.
  */
 /*
- * $Id: amtape.c,v 1.22.2.6.2.1 2001/01/24 22:12:17 jrjackson Exp $
+ * $Id: amtape.c,v 1.22.2.6.2.2 2001/02/28 00:34:45 jrjackson Exp $
  *
  * tape changer interface program
  */
@@ -261,12 +261,18 @@ int argc;
 char **argv;
 {
     char *slotstr = NULL, *devicename = NULL;
+    char *errstr;
 
     if(argc != 2)
 	usage();
 
     if(changer_loadslot(argv[1], &slotstr, &devicename)) {
 	error("could not load slot %s: %s", slotstr, changer_resultstr);
+    }
+    if((errstr = tape_rewind(devicename)) != NULL) {
+	fprintf(stderr,
+		"%s: could not rewind %s: %s", get_pname(), devicename, errstr);
+	amfree(errstr);
     }
 
     fprintf(stderr, "%s: changed to slot %s on %s\n",
@@ -318,6 +324,12 @@ char *device;
 	    fprintf(stderr, " (wrong tape)\n");
 	else {
 	    fprintf(stderr, " (exact label match)\n");
+	    if((errstr = tape_rewind(device)) != NULL) {
+		fprintf(stderr,
+			"%s: could not rewind %s: %s",
+			get_pname(), device, errstr);
+		amfree(errstr);
+	    }
 	    found = 1;
 	    amfree(datestamp);
 	    amfree(label);
@@ -343,7 +355,7 @@ char **argv;
 
     found = 0;
 
-    changer_find(scan_init, loadlabel_slot,searchlabel);
+    changer_find(scan_init, loadlabel_slot, searchlabel);
 
     if(found)
 	fprintf(stderr, "%s: label %s is now loaded.\n",
