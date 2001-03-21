e1f520c189e0553d1f1cb5c4f1d92082675cad75
John R. Jackson <jrj@gandalf.cc.purdue.edu>
2001-03-21 20:57:04 +0000

    Fix problem that caused xfsrestore arg list to not be NULL terminated.

--- recover-src/extract_list.c
+++ recover-src/extract_list.c
@@ -24,7 +24,7 @@
  * file named AUTHORS, in the root directory of this distribution.
  */
 /*
- * $Id: extract_list.c,v 1.60 2001/03/13 21:28:47 jrjackson Exp $
+ * $Id: extract_list.c,v 1.61 2001/03/21 20:57:04 jrjackson Exp $
  *
  * implements the "extract" command in amrecover
  */
@@ -1334,12 +1334,10 @@ static void extract_files_child(in_fd, elist)
     if (strcmp(file.program, XFSDUMP) == 0) {
 	restore_args[j++] = stralloc("-");
 	restore_args[j++] = stralloc(".");
-    } else
-#endif
-    {
-    restore_args[j] = NULL;
     }
-  
+#endif
+    restore_args[j] = NULL;
+
     switch (dumptype) {
     case IS_SAMBA:
 #ifdef SAMBA_CLIENT
