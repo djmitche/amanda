8a0ecb9a689bc375f2e97195abd7ffe4954a296e
John R. Jackson <jrj@gandalf.cc.purdue.edu>
2001-04-23 21:11:10 +0000

    Allow Samba shares with no password.

--- client-src/sendbackup-gnutar.c
+++ client-src/sendbackup-gnutar.c
@@ -24,7 +24,7 @@
  * file named AUTHORS, in the root directory of this distribution.
  */
 /* 
- * $Id: sendbackup-gnutar.c,v 1.56.2.16 2001/03/15 02:17:34 jrjackson Exp $
+ * $Id: sendbackup-gnutar.c,v 1.56.2.17 2001/04/23 21:11:10 jrjackson Exp $
  *
  * send backup data using GNU tar
  */
@@ -321,6 +321,8 @@ static void start_backup(host, disk, level, dumpdate, dataf, mesgf, indexf)
 	char *taropt;
 	int passwdf;
 	int lpass;
+	int pwtext_len;
+	char *pw_fd_env;
 
 	if ((user_and_password = findpass(disk, &domain)) == NULL) {
 	    if(domain) {
@@ -341,6 +343,7 @@ static void start_backup(host, disk, level, dumpdate, dataf, mesgf, indexf)
 		  get_pname(), disk);
 	}
 	*pwtext++ = '\0';
+	pwtext_len = strlen(pwtext);
 	if ((sharename = makesharename(disk, 0)) == 0) {
 	    memset(user_and_password, '\0', lpass);
 	    amfree(user_and_password);
@@ -373,12 +376,18 @@ static void start_backup(host, disk, level, dumpdate, dataf, mesgf, indexf)
 
 	start_index(createindex, dumpout, mesgf, indexf, indexcmd);
 
+	if (pwtext_len > 0) {
+	    pw_fd_env = "PASSWD_FD";
+	} else {
+	    pw_fd_env = "dummy_PASSWD_FD";
+	}
 	dumppid = pipespawn(cmd, STDIN_PIPE|PASSWD_PIPE,
 			    &dumpin, &dumpout, &mesgf,
-			    "PASSWD_FD", &passwdf,
+			    pw_fd_env, &passwdf,
 			    "smbclient",
 			    sharename,
-			    "-U", user_and_password,		/* user only */
+			    *user_and_password ? "-U" : skip_argument,
+			    *user_and_password ? user_and_password : skip_argument,
 			    "-E",
 			    domain ? "-W" : skip_argument,
 			    domain ? domain : skip_argument,
@@ -391,7 +400,7 @@ static void start_backup(host, disk, level, dumpdate, dataf, mesgf, indexf)
 	    memset(domain, '\0', strlen(domain));
 	    amfree(domain);
 	}
-	if(fullwrite(passwdf, pwtext, strlen(pwtext)) < 0) {
+	if(pwtext_len > 0 && fullwrite(passwdf, pwtext, pwtext_len) < 0) {
 	    int save_errno = errno;
 
 	    aclose(passwdf);
