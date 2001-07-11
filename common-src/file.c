1079dda528f78438764c82e3006e6c8dada884be
John R. Jackson <jrj@gandalf.cc.purdue.edu>
2001-07-11 20:56:13 +0000

    Fix a memory leak.
    Grab exponentially larger chunks of memory up to a point,
    then level out.

--- common-src/file.c
+++ common-src/file.c
@@ -23,7 +23,7 @@
  * Author: AMANDA core development group.
  */
 /*
- * $Id: file.c,v 1.24 2001/03/20 00:27:18 jrjackson Exp $
+ * $Id: file.c,v 1.25 2001/07/11 20:56:13 jrjackson Exp $
  *
  * file and directory bashing routines
  */
@@ -396,8 +396,12 @@ agets(file)
 	}
 	line_len += line_free - 1;		/* bytes read minus '\0' */
 	size_save = line_size;
-	line_size += AGETS_LINE_INCR;		/* get more space */
-	cp = alloc (line_size);
+	if (line_size < 256 * AGETS_LINE_INCR) {
+	    line_size *= 2;
+	} else {
+	    line_size += 256 * AGETS_LINE_INCR;
+	}
+	cp = alloc (line_size);			/* get more space */
 	memcpy (cp, line, size_save);		/* copy old to new */
 	free (line);				/* and release the old */
 	line = cp;
@@ -561,9 +565,15 @@ areads (fd)
 	 * No newline yet, so get more data.
 	 */
 	if (buflen == 0) {
-	    size = areads_buffer[fd].bufsize + areads_bufsize;
+	    if ((size = areads_buffer[fd].bufsize) < 256 * areads_bufsize) {
+		size *= 2;
+	    } else {
+		size += 256 * areads_bufsize;
+	    }
 	    newbuf = alloc(size + 1);
 	    memcpy (newbuf, buffer, areads_buffer[fd].bufsize + 1);
+	    amfree(areads_buffer[fd].buffer);
+	    buffer = NULL;
 	    areads_buffer[fd].buffer = newbuf;
 	    areads_buffer[fd].endptr = newbuf + areads_buffer[fd].bufsize;
 	    areads_buffer[fd].bufsize = size;
