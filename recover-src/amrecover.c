270ef91fda4f7f409ad539826280a5bc3400b151
John R. Jackson <jrj@gandalf.cc.purdue.edu>
2001-07-30 20:14:24 +0000

    Improve the amrecover "Unexpected server end of file" message.

--- recover-src/amrecover.c
+++ recover-src/amrecover.c
@@ -24,7 +24,7 @@
  * file named AUTHORS, in the root directory of this distribution.
  */
 /*
- * $Id: amrecover.c,v 1.39 2001/06/19 19:36:04 jrjackson Exp $
+ * $Id: amrecover.c,v 1.40 2001/07/30 20:14:24 jrjackson Exp $
  *
  * an interactive program for recovering backed-up files
  */
@@ -123,8 +123,9 @@ int get_line ()
 				get_pname(),
 				strerror(save_errno));
 	    } else {
-		fprintf(stderr, "%s: Unexpected server end of file\n",
-			get_pname());
+		fprintf(stderr, "%s: Unexpected end of file, check amindexd*debug on server %s\n",
+			get_pname(),
+			server_name);
 	    }
 	    amfree(line);
 	    amfree(server_line);
