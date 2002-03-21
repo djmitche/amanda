0af36a54241eb6e8f61b82a726d4e6fd7569d35b
Jean-Louis Martineau <martineau@zmanda.com>
2002-03-21 19:50:58 +0000

    * server-src/driver.c (dump_to_tape): Call start_degraded_mode() if   taper failed.

--- server-src/driver.c
+++ server-src/driver.c
@@ -24,7 +24,7 @@
  * file named AUTHORS, in the root directory of this distribution.
  */
 /*
- * $Id: driver.c,v 1.58.2.31.2.8.2.6 2002/01/31 18:24:45 martinea Exp $
+ * $Id: driver.c,v 1.58.2.31.2.8.2.7 2002/03/21 19:50:58 martinea Exp $
  *
  * controlling process for the Amanda backup system
  */
@@ -2189,6 +2189,7 @@ int dump_to_tape(dp)
 	update_failed_dump_to_tape(dp);
 	free_serial(result_argv[2]);
 	failed = 2;	/* fatal problem */
+	start_degraded_mode(&runq);
     }
 
     /* reset statistics & return */
