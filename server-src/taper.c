76bd3932b0e1519620dd7a74d7903d51abdeb69c
Jean-Louis Martineau <martineau@zmanda.com>
2002-05-02 20:36:17 +0000

    * server-src/taper.c (detach_buffers): Fix compilation with mmap.

--- server-src/taper.c
+++ server-src/taper.c
@@ -23,7 +23,7 @@
  * Authors: the Amanda Development Team.  Its members are listed in a
  * file named AUTHORS, in the root directory of this distribution.
  */
-/* $Id: taper.c,v 1.47.2.14.4.8.2.7 2002/04/17 20:05:42 martinea Exp $
+/* $Id: taper.c,v 1.47.2.14.4.8.2.8 2002/05/02 20:36:17 martinea Exp $
  *
  * moves files from holding disk to tape, or from a socket to tape
  */
@@ -1553,7 +1553,7 @@ char *attach_buffers(size)
 void detach_buffers(bufp)
 char *bufp;
 {
-    if(munmap((void *)bufp, saved_size)*conf_tapebufs) == -1) {
+    if(munmap((void *)bufp, saved_size) == -1) {
 	error("detach_buffers: munmap: %s", strerror(errno));
     }
 
