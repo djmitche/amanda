5a1113f1ea74823c253ae2c4b862a2ec0a18e5ff
Jean-Louis Martineau <martineau@zmanda.com>
2002-11-26 22:51:09 +0000

    * server-src/amflush.c: Write a "amflush: start at ???" line. * server-src/amstatus.pl.in: parse the "amflush: start at ???" line.

--- server-src/amflush.c
+++ server-src/amflush.c
@@ -24,7 +24,7 @@
  * file named AUTHORS, in the root directory of this distribution.
  */
 /*
- * $Id: amflush.c,v 1.73 2002/11/05 01:58:52 martinea Exp $
+ * $Id: amflush.c,v 1.74 2002/11/26 22:51:09 martinea Exp $
  *
  * write files from work directory onto tape
  */
@@ -80,6 +80,8 @@ char **main_argv;
     sl_t *holding_list=NULL;
     sle_t *holding_file;
     int driver_pipe[2];
+    char date_string[100];
+    time_t today;
 
     for(fd = 3; fd < FD_SETSIZE; fd++) {
 	/*
@@ -210,6 +212,9 @@ char **main_argv;
     if(!foreground) detach();
     erroutput_type = (ERR_AMANDALOG|ERR_INTERACTIVE);
     set_logerror(logerror);
+    today = time(NULL);
+    strftime(date_string, 100, "%a %b %e %H:%M:%S %Z %Y", localtime (&today));
+    fprintf(stderr, "amflush: start at %s\n", date_string);
     fprintf(stderr, "amflush: datestamp %s\n", datestamp);
     log_add(L_START, "date %s", datestamp);
 
