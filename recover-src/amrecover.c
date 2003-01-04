25d8a8c282ad5807aec732ed3ec825f0879b52da
Jean-Louis Martineau <martineau@zmanda.com>
2003-01-04 04:33:32 +0000

    * recover-src/amrecover.c: Fix memory leak.

--- recover-src/amrecover.c
+++ recover-src/amrecover.c
@@ -24,7 +24,7 @@
  * file named AUTHORS, in the root directory of this distribution.
  */
 /*
- * $Id: amrecover.c,v 1.29.4.7.4.6.2.6 2003/01/01 23:28:52 martinea Exp $
+ * $Id: amrecover.c,v 1.29.4.7.4.6.2.7 2003/01/04 04:33:32 martinea Exp $
  *
  * an interactive program for recovering backed-up files
  */
@@ -640,6 +640,7 @@ char **argv;
 		    set_directory(cwd);
 		    if (server_happy() && strcmp(cwd, mpt_guess) != 0)
 		        printf("WARNING: not on root of selected filesystem, check man-page!\n");
+		    amfree(dn_guess);
 		    amfree(mpt_guess);
 		    break;
 
