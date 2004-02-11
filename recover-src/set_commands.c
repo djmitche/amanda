39b04b3576986f9222dae808ca7e52fc6d9142f6
Jean-Louis Martineau <martineau@zmanda.com>
2004-02-11 13:15:18 +0000

    * recover-src/set_commands.c (cd_glob): Fix memory leak.

--- recover-src/set_commands.c
+++ recover-src/set_commands.c
@@ -24,7 +24,7 @@
  * file named AUTHORS, in the root directory of this distribution.
  */
 /*
- * $Id: set_commands.c,v 1.22 2004/02/11 13:03:29 martinea Exp $
+ * $Id: set_commands.c,v 1.23 2004/02/11 13:15:18 martinea Exp $
  *
  * implements the "set" commands in amrecover
  */
@@ -247,6 +247,7 @@ char *glob;
     if ((s = validate_regexp(regex)) != NULL) {
         printf("\"%s\" is not a valid shell wildcard pattern: ", glob);
         puts(s);
+	amfree(regex);
         return;
     }
     /*
