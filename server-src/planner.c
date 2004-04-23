ec72cfd87be8d27e8171693169dce67c8b50cf5c
Jean-Louis Martineau <martineau@zmanda.com>
2004-04-23 11:38:49 +0000

    * server-src/planner.c (setup_estimate): Typo.

--- server-src/planner.c
+++ server-src/planner.c
@@ -24,7 +24,7 @@
  * file named AUTHORS, in the root directory of this distribution.
  */
 /*
- * $Id: planner.c,v 1.155 2004/04/22 19:22:07 martinea Exp $
+ * $Id: planner.c,v 1.156 2004/04/23 11:38:49 martinea Exp $
  *
  * backup schedule planner for the Amanda backup system.
  */
@@ -829,7 +829,7 @@ setup_estimate(dp)
 	}
     }
 
-    if(!dp->skip_incr && !dp->strategy == DS_NOINC) {
+    if(!dp->skip_incr && !(dp->strategy == DS_NOINC)) {
 	if(ep->last_level == -1) {		/* a new disk */
 	    if(dp->strategy == DS_NOFULL || dp->strategy == DS_INCRONLY) {
 		askfor(ep, i++, 1, &info);
