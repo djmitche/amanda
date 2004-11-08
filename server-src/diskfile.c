a984a6388fe1f2205dedf38018a935aa78d08814
Jean-Louis Martineau <martineau@zmanda.com>
2004-11-08 18:31:04 +0000

    * server-src/diskfile.c (read_diskline): A spindle must be numerical.

--- server-src/diskfile.c
+++ server-src/diskfile.c
@@ -25,7 +25,7 @@
  *			   University of Maryland at College Park
  */
 /*
- * $Id: diskfile.c,v 1.60 2004/08/03 11:27:05 martinea Exp $
+ * $Id: diskfile.c,v 1.61 2004/11/08 18:31:04 martinea Exp $
  *
  * read disklist file
  */
@@ -460,6 +460,22 @@ parse_diskline(lst, filename, diskf, line_num_p, line_p)
     skip_whitespace(s, ch);
     fp = s - 1;
     if(ch && ch != '#') {		/* get optional spindle number */
+	char *fp1;
+	int is_digit=1;
+	skip_non_whitespace(s, ch);
+	s[-1] = '\0';
+	for(fp1=fp;*fp1!='\0';fp1++) {
+	    if(!isdigit(*fp1)) {
+		is_digit = 0;
+	    }
+	}
+	if(is_digit == 0) {
+	    parserror("non-integer spindle `%s'", fp);
+	    if(host == NULL) amfree(hostname);
+	    amfree(disk->name);
+	    amfree(disk);
+	    return 1;
+	}
 	disk->spindle = atoi(fp);
 	skip_integer(s, ch);
     }
