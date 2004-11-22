9fe48b247f2f9992048259184a535d3ea98f4ea7
Jean-Louis Martineau <martineau@zmanda.com>
2004-11-22 13:32:31 +0000

    * server-src/driver.c: Typo.

--- server-src/driver.c
+++ server-src/driver.c
@@ -24,7 +24,7 @@
  * file named AUTHORS, in the root directory of this distribution.
  */
 /*
- * $Id: driver.c,v 1.145 2004/11/19 18:09:17 martinea Exp $
+ * $Id: driver.c,v 1.146 2004/11/22 13:32:31 martinea Exp $
  *
  * controlling process for the Amanda backup system
  */
@@ -546,7 +546,7 @@ startaflush()
 	fprintf(stderr,"driver: startaflush: %s %s %s %ld %ld\n",
 		taperalgo2str(conf_taperalgo), dp->host->hostname,
 		dp->name, sched(taper_disk)->act_size, tape_left);
-	if(sched(dp)->act_size > tape_left)
+	if(sched(dp)->act_size <= tape_left)
 	    tape_left -= sched(dp)->act_size;
 	else
 	    tape_left = 0;
