4a1db31a898ea8b3e874b78aadf5c6046f2a4da9
Jean-Louis Martineau <martineau@zmanda.com>
2006-04-14 20:44:06 +0000

    * recover-src/help.c: Document setdate YYYY-MM-DD-HH-MM-SS format. * man/xml-source/amrecover.8.xml: Ditto.

--- recover-src/help.c
+++ recover-src/help.c
@@ -24,7 +24,7 @@
  * file named AUTHORS, in the root directory of this distribution.
  */
 /*
- * $Id: help.c,v 1.8 2002/03/06 19:23:20 martinea Exp $
+ * $Id: help.c,v 1.9 2006/04/14 20:44:06 martinea Exp $
  *
  * implements the "help" command in amrecover
  */
@@ -54,11 +54,12 @@ void help_list P((void))
     printf("mode              - show the method used to extract SMB shares\n");
     printf("pwd               - show cwd on virtual file system\n");
     printf("quit\n");
-    printf("listdisk [diskdevice] - list disks\n");
+    printf("listdisk [diskdevice]              - list disks\n");
     printf("setdate {YYYY-MM-DD|--MM-DD|---DD} - set date of look\n");
-    printf("setdisk diskname [mountpoint] - select disk on dump host\n");
-    printf("sethost host      - select dump host\n");
-    printf("settape [host:][device|default] - select tape server and/or device\n");
-    printf("setmode smb|tar   - select the method used to extract SMB shares\n");
+    printf("        {YYYY-MM-DD-HH-MM-SS}      - set date of look\n");
+    printf("setdisk diskname [mountpoint]      - select disk on dump host\n");
+    printf("sethost host                       - select dump host\n");
+    printf("settape [host:][device|default]    - select tape server and/or device\n");
+    printf("setmode smb|tar                 - select the method used to extract SMB shares\n");
     printf("\n");
 }
