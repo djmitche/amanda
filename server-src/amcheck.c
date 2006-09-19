59eaa558b963282dd9f556fb1edfee532dc63eb0
Jean-Louis Martineau <martineau@zmanda.com>
2006-09-19 19:07:56 +0000

    * server-src/amcheck.c: Print "tapelist" instead of "tapefile".

--- server-src/amcheck.c
+++ server-src/amcheck.c
@@ -24,7 +24,7 @@
  * file named AUTHORS, in the root directory of this distribution.
  */
 /*
- * $Id: amcheck.c,v 1.149 2006/08/24 01:57:16 paddy_s Exp $
+ * $Id: amcheck.c,v 1.149.2.1 2006/09/19 19:07:56 martinea Exp $
  *
  * checks for common problems in server and clients
  */
@@ -764,7 +764,7 @@ start_server_check(
 	}
 	else if(stat(tapefile, &statbuf) == -1) {
 	    quoted = quote_string(tape_dir);
-	    fprintf(outf, "ERROR: tapefile %s (%s), "
+	    fprintf(outf, "ERROR: tapelist %s (%s), "
 		    "you must create an empty file.\n",
 		    quoted, strerror(errno));
 	    tapebad = 1;
@@ -772,24 +772,24 @@ start_server_check(
 	}
 	else if(!S_ISREG(statbuf.st_mode)) {
 	    quoted = quote_string(tapefile);
-	    fprintf(outf, "ERROR: tapefile %s: should be a regular file.\n",
+	    fprintf(outf, "ERROR: tapelist %s: should be a regular file.\n",
 		    quoted);
 	    tapebad = 1;
 	    amfree(quoted);
 	}
 	else if(access(tapefile, F_OK) != 0) {
 	    quoted = quote_string(tapefile);
-	    fprintf(outf, "ERROR: can't access tape list %s\n", quoted);
+	    fprintf(outf, "ERROR: can't access tapelist %s\n", quoted);
 	    tapebad = 1;
 	    amfree(quoted);
 	} else if(access(tapefile, F_OK) == 0 && access(tapefile, W_OK) != 0) {
 	    quoted = quote_string(tapefile);
-	    fprintf(outf, "ERROR: tape list %s: not writable\n", quoted);
+	    fprintf(outf, "ERROR: tapelist %s: not writable\n", quoted);
 	    tapebad = 1;
 	    amfree(quoted);
 	} else if(read_tapelist(tapefile)) {
 	    quoted = quote_string(tapefile);
-	    fprintf(outf, "ERROR: tape list %s: parse error\n", quoted);
+	    fprintf(outf, "ERROR: tapelist %s: parse error\n", quoted);
 	    tapebad = 1;
 	    amfree(quoted);
 	}
