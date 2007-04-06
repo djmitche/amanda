eedc90cf9a34de21bcb4793541aeab310c99bbd6
Jean-Louis Martineau <martineau@zmanda.com>
2007-04-06 17:09:58 +0000

    * server-src/taper.c: Limit splitsize to 1G in PORT-WRITE.

--- server-src/taper.c
+++ server-src/taper.c
@@ -23,7 +23,7 @@
  * Authors: the Amanda Development Team.  Its members are listed in a
  * file named AUTHORS, in the root directory of this distribution.
  */
-/* $Id: taper.c,v 1.144.2.10 2007/02/22 18:28:35 martinea Exp $
+/* $Id: taper.c,v 1.144.2.11 2007/04/06 17:09:58 martinea Exp $
  *
  * moves files from holding disk to tape, or from a socket to tape
  */
@@ -861,6 +861,9 @@ file_reader_side(
 		/*NOTREACHED*/
 	    }
 	    splitsize = OFF_T_ATOI(cmdargs.argv[a++]);
+	    if (SIZEOF_OFF_T == 4 && splitsize > 1048576) { /* 1G in 32 bits */
+		splitsize = 1048576;
+	    }
 
 	    if (a >= cmdargs.argc) {
 		error("error [taper PORT-WRITE: not enough args: split_diskbuffer]");
@@ -874,6 +877,9 @@ file_reader_side(
 	    }
 	    /* Must fit in memory... */
 	    fallback_splitsize = (size_t)atoi(cmdargs.argv[a++]);
+	    if (SIZEOF_OFF_T == 4 && fallback_splitsize > 1048576) { /* 1G */
+		fallback_splitsize = 1048576;
+	    }
 
 	    if (a != cmdargs.argc) {
 		error("error [taper file_reader_side PORT-WRITE: too many args: %d != %d]",
@@ -2473,6 +2479,8 @@ write_file(void)
     return;
 
  tape_error:
+    if (errstr) 
+	dbprintf("tape_error: %s\n", errstr);
     /* got tape error */
     if (next_tape(1)) {
 	if (syncpipe_put('T', 0) == -1) {   /* next tape in place, try again */
